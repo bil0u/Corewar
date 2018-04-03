7a29107ce5fa737ef8b240c3d45ccb482e43fa11
upopee <upopee@student.42.fr>
2018-04-03 04:49:33 +0200

    Added sound intergration

--- Test SDL/sdl_test.c
+++ Test SDL/sdl_test.c
@@ -6,15 +6,24 @@
 /*   By: upopee <upopee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/31 15:04:17 by upopee            #+#    #+#             */
-/*   Updated: 2018/04/02 21:05:32 by upopee           ###   ########.fr       */
+/*   Updated: 2018/04/03 04:48:58 by upopee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <SDL/SDL.h>
-#include <SDL/SDL_image.h>
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_mixer.h"
 #include "sdl_test.h"
+
+void	exit_error(char *prompt, char *err_msg)
+{
+	printf("%s: %s\n", prompt, err_msg);
+	Mix_CloseAudio();
+	SDL_Quit();
+	exit(EXIT_FAILURE);
+}
 
 void	init_window(SDL_Surface **screen)
 {
@@ -23,24 +32,32 @@ void	init_window(SDL_Surface **screen)
 
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != -1)
 	{
-		icon = IMG_Load(LOGO_PATH);
+		icon = IMG_Load(IMG_LOGO);
 		SDL_WM_SetIcon(icon, NULL);
 		SDL_FreeSurface(icon);
 		win_flags = SDL_HWSURFACE | SDL_RESIZABLE | SDL_DOUBLEBUF;
 		*screen = SDL_SetVideoMode(WINSZ_X, WINSZ_Y, 32, win_flags);
 		if (*screen != NULL)
+		{
 			SDL_WM_SetCaption(WIN_NAME, NULL);
+			if (Mix_OpenAudio(DEF_FREQ, MIX_DEFAULT_FORMAT, 2, CHK_SIZE) == -1)
+				exit_error("Mix_OpenAudio()", Mix_GetError());
+		}
 		else
-			printf("Impossible d'initialiser la fenetre: %s\n", SDL_GetError());
+			exit_error("SDL_SetVideoMode()", SDL_GetError());
 	}
 	else
-		printf("Impossible d'initialiser SDL: %s\n", SDL_GetError());
+		exit_error("SDL_Init()", SDL_GetError());
 }
+
+/*
+** ----------------------------- IMAGES ---------------------------------------
+*/
 
 void 	init_images(SDL_Surface **images)
 {
-	images[0] = IMG_Load(BG_PATH);
-	images[1] = IMG_Load(LOGO_PATH);
+	images[0] = IMG_Load(IMG_BG);
+	images[1] = NULL; //IMG_Load(IMG_LOGO);
 	images[2] = NULL;
 }
 
@@ -69,12 +86,48 @@ void	del_images(SDL_Surface **images)
 		SDL_FreeSurface(images[i]);
 }
 
+/*
+** ----------------------------- AUDIO ----------------------------------------
+*/
+
+void	init_audio(Mix_Music **music, Mix_Chunk **effects)
+{
+	Mix_VolumeMusic(MIX_MAX_VOLUME >> 1);
+	*music = Mix_LoadMUS(MUS_OPENING);
+	Mix_AllocateChannels(NB_EFFECTS);
+	Mix_Volume(0, MIX_MAX_VOLUME >> 1);
+	effects[0] = Mix_LoadWAV(EFF_HEADSHOT);
+	Mix_Volume(1, MIX_MAX_VOLUME >> 1);
+	effects[1] = Mix_LoadWAV(EFF_SNIPER);
+	Mix_Volume(2, MIX_MAX_VOLUME >> 1);
+	effects[2] = Mix_LoadWAV(EFF_MONSTERK);
+	effects[NB_EFFECTS] = NULL;
+}
+
+void	del_audio(Mix_Music *music, Mix_Chunk **effects)
+{
+	int		i;
+
+	i = -1;
+	while (effects[++i] != NULL)
+		Mix_FreeChunk(effects[i]);
+	Mix_FreeMusic(music);
+}
+
+/*
+** ----------------------------------------------------------------------------
+*/
+
 void	main_loop(SDL_Surface *screen)
 {
-	SDL_Surface	*images[3];
+	SDL_Surface	*images[NB_IMAGES + 1];
+	Mix_Music	*music;
+	Mix_Chunk	*effects[NB_EFFECTS + 1];
 	SDL_Event	event;
 
 	init_images(images);
+	init_audio(&music, effects);
+	Mix_PlayMusic(music, -1);
 	while (42)
 	{
 		put_images(screen, images);
@@ -83,9 +136,21 @@ void	main_loop(SDL_Surface *screen)
 		if (event.type == SDL_QUIT)
 			break ;
 		else if (event.type == SDL_KEYDOWN)
+		{
 			if (event.key.keysym.sym == SDLK_ESCAPE)
 				break ;
+			else if (event.key.keysym.sym == SDLK_SPACE)
+				Mix_PausedMusic() ? Mix_ResumeMusic() : Mix_PauseMusic();
+			else if (event.key.keysym.sym == SDLK_1)
+				Mix_PlayChannel(0, effects[0], 0);
+			else if (event.key.keysym.sym == SDLK_2)
+				Mix_PlayChannel(1, effects[1], 0);
+			else if (event.key.keysym.sym == SDLK_3)
+				Mix_PlayChannel(2, effects[2], 0);
+		}
 	}
+	del_images(images);
+	del_audio(music, effects);
 }
 
 int		main(int argc, char* argv[])
@@ -96,7 +161,10 @@ int		main(int argc, char* argv[])
 	init_window(&screen);
 	if (screen == NULL)
 		return (EXIT_FAILURE);
+
 	main_loop(screen);
+
+	Mix_CloseAudio();
 	SDL_Quit();
 	return (EXIT_SUCCESS);
 }
--- Test SDL/sdl_test.h
+++ Test SDL/sdl_test.h
@@ -6,18 +6,28 @@
 /*   By: upopee <upopee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/31 20:20:36 by upopee            #+#    #+#             */
-/*   Updated: 2018/03/31 21:33:21 by upopee           ###   ########.fr       */
+/*   Updated: 2018/04/03 04:47:28 by upopee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SDL_TEST_H
 # define SDL_TEST_H
 
-# define LOGO_PATH		"images/CW_logo.png"
-# define BG_PATH		"images/background.png"
+# define NB_IMAGES		2
+# define IMG_LOGO		"images/CW_logo.png"
+# define IMG_BG			"images/background.png"
+
+# define NB_EFFECTS		3
+# define MUS_OPENING	"audio/opening.ogg"
+# define EFF_HEADSHOT	"audio/headshot.wav"
+# define EFF_SNIPER		"audio/sniperfire.wav"
+# define EFF_MONSTERK	"audio/monsterkill.wav"
 
 # define WIN_NAME		"Fenêtre de test"
 # define WINSZ_X		1280
 # define WINSZ_Y		960
+
+# define DEF_FREQ		44100
+# define CHK_SIZE		4096
 
 #endif
